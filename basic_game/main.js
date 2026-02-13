35317b751b7e40fe3ee935a5bff404cac68d4684
nguyenquocbao1912 <144327913+nguyenquocbao1912@users.noreply.github.com>
2026-02-14 00:05:21 +0700

    Delete folder

--- basic_game/main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './Game.js';
-
-window.addEventListener('load', () => {
-    const canvas = document.getElementById('gameCanvas');
-    const game = new Game(canvas);
-    game.start();
-});
